6f4bf52d5f7bb8b588b1ecffa394e80d01c10789
Trenton <trenton.vanderwert@suse.com>
2022-06-17 12:13:22 -0500

    began making an easier script with more being leveraged on API and called it v2

--- globalrb-admin-hack.sh
+++ globalrb-admin-hack.sh
@@ -11,39 +11,62 @@ bash -c "$CLEAN_UP"
 
 # create the user crb config for downstream cluster
 kubectl get globalrolebinding -o jsonpath='{range .items[*]}{@.userName}{" "}{@.globalRoleName}{"\n"}{end}' | awk "/$ROLE/ {print \$1}" | while read name; do 
-cat <<EOF > $MANIFEST_FILE 
-apiVersion: rbac.authorization.k8s.io/v1
-kind: ClusterRoleBinding
-metadata:
-  name: customadmin-user-$name
-roleRef:
-  apiGroup: rbac.authorization.k8s.io
-  kind: ClusterRole
-  name: cluster-admin
-subjects:
-- apiGroup: rbac.authorization.k8s.io
-  kind: User
-  name: $name
+cat <<-EOF > $MANIFEST_FILE 
+	apiVersion: rbac.authorization.k8s.io/v1
+	kind: ClusterRoleBinding
+	metadata:
+  	  name: customadmin-user-$name
+	roleRef:
+  	  apiGroup: rbac.authorization.k8s.io
+  	  kind: ClusterRole
+  	  name: cluster-admin
+	subjects:
+	- apiGroup: rbac.authorization.k8s.io
+	  kind: User
+	  name: $name
 
----
-  
+	---
 EOF
+  cat $MANIFEST_FILE >> static-config.yaml 
 
-  cat $MANIFEST_FILE >> static-config.yaml
   # format yaml for post request
   PAYLOAD_STRING=$(awk '{printf "%s\\n", $0}' $MANIFEST_FILE | sed 's/^/{"yaml": "/;s/$/"}/');
 
   # Get a list of all not local clusters hosted by Rancher
   kubectl get clusters.management.cattle.io --no-headers | cut -d ' ' -f1 | grep -v local | while read clusters; do
+    # apply the local CRB to give rancher permissions to the cluster.
+read -r -d '' RANCHER_RBAC <<-EOF
+	apiVersion: rbac.authorization.k8s.io/v1
+	kind: ClusterRoleBinding
+	metadata:
+	  name: $clusters-custom-admin-crb-clusters-u-t4bgj
+	roleRef:
+	  apiGroup: rbac.authorization.k8s.io
+	  kind: ClusterRole
+	  name: $clusters-restricted-admin-cr-clusters
+	subjects:
+	- apiGroup: rbac.authorization.k8s.io
+	  kind: User
+	  name: $name
+EOF
     # Post to the Rancher API with the dsuserconfig.yaml
     create_rbac="curl -k -X POST -u $ACCESS_TOKEN -d '$PAYLOAD_STRING' https://$RANCHER_HOST/v1/management.cattle.io.clusters/$clusters?action=apply"
     if [ "$1" == "-a" ]
     then
       # run the script
+      echo " creating local manifest"
+      echo "$RANCHER_RBAC" | kubectl apply -f -
       echo "running: $create_rbac"
       bash -c "$create_rbac"
     else
-      echo "staged curl command: $create_rbac"
+      echo "staged manifest:"
+      echo ""
+      echo "$RANCHER_RBAC"
+      echo ""
+      echo "staged curl command:"
+      echo ""
+      echo "$create_rbac"
+      echo ""
     fi;
   done;
 done;
---
+++ globalrb-admin-hack_v2.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+ROLE=''
+
+kubectl get globalrolebinding -o jsonpath='{range .items[*]}{@.userName}{" "}{@.globalRoleName}{"\n"}{end}' | awk "/$ROLE/ {print \$1}" | while read user; do
+  kubectl get clusters.management.cattle.io --no-headers | cut -d ' ' -f1 | grep -v local | while read cluster; do
+    read -r -d '' MANIFEST <<-EOF
+	apiVersion: management.cattle.io/v3
+	clusterName: $cluster
+	kind: ClusterRoleTemplateBinding
+	metadata:
+	  name: $user-admin
+	  namespace: $cluster
+	roleTemplateName: cluster-owner
+	userName: $user
+	EOF
+    if [ "$1" == "-a" ]; then
+      echo "applying manifest!"
+      echo "$MANIFEST" | kubectl apply -f -
+    else
+      echo "$MANIFEST"
+    fi;
+  done;
+done;
