79b14b28bcffc67f9283740fa66a74e008870322
Trent <trenton.vanderwert@gmail.com>
2022-06-23 14:40:55 -0500

    added logic to prevent duplicate rb creation

--- globalrb-admin-hack_v2.sh
+++ globalrb-admin-hack_v2.sh
@@ -4,7 +4,8 @@ ROLE=''
 
 kubectl get globalrolebindings.management.cattle.io -o jsonpath='{range .items[*]}{@.userName}{" "}{@.globalRoleName}{"\n"}{end}' | awk "/$ROLE/ {print \$1}" | while read user; do
   kubectl get clusters.management.cattle.io --no-headers | cut -d ' ' -f1 | grep -v local | while read cluster; do
-    read -r -d '' MANIFEST <<-EOF
+    if ! kubectl get clusterroletemplatebinding -n $cluster "$user-admin" &>/dev/null; then	    
+      read -r -d '' MANIFEST <<-EOF
 	apiVersion: management.cattle.io/v3
 	clusterName: $cluster
 	kind: ClusterRoleTemplateBinding
@@ -14,11 +15,14 @@ kubectl get globalrolebindings.management.cattle.io -o jsonpath='{range .items[*
 	roleTemplateName: cluster-owner
 	userName: $user
 	EOF
-    if [ "$1" == "-a" ]; then
-      echo "applying manifest!"
-      echo "$MANIFEST" | kubectl apply -f -
+      if [ "$1" == "-a" ]; then
+        echo "applying manifest!";
+        echo "$MANIFEST" | kubectl apply -f -;
+      else
+        echo "$MANIFEST";
+      fi;
     else
-      echo "$MANIFEST"
+      echo "$user is already set up";
     fi;
   done;
 done;
